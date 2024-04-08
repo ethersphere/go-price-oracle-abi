fe4b8c0b200e0bf4b055a47cddbd1521b1841e32
bee-worker <bee-worker@ethswarm.org>
2024-04-08 15:14:43 +0000

    generate code for price oracle contract v0.6.6-rc2

---
+++ priceoracleabi/abi_v0.6.6-rc2.go
@@ -0,0 +1,9 @@
+// Copyright 2021 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package priceoracleabi
+
+const PriceOracleABIv0_6_6-rc2 = ``
+const PriceOracleBinv0_6_6-rc2 = 
+const PriceOracleDeployedBinv0_6_6-rc2 = 
